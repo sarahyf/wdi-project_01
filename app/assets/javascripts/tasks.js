2075e5dea1c907f0b74253646201003f22d180c4
sarahyf <alfadlysarah@gmail.com>
2018-11-14 00:30:53 +0300

    updated project

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -9,3 +9,15 @@ jQuery.fn.submitOnCheck = function () {
 $(document).on("turbolinks:load", function (){
     $('.edit_task').submitOnCheck();
 });
+
+// jQuery.fn.submitOnCheck = function () {
+//     this.find('input[type=submit]').remove();
+//     this.find('input[type=checkbox]').click(function () {
+//         $(this).parent('form').submit();
+//     });
+//     return this;
+// }
+
+// $(function () {
+//     $('.edit_task').submitOnCheck();
+// })
